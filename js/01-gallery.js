4b9b60fa7fb5b1bd59139d74e01f50f863cfd064
Makoto-kino14 <madina14madlen@gmail.com>
2022-09-29 22:55:18 +0600

    Ver 1.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imagesMarkup = createImgGallery(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImgGallery(items) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+            <div class='gallery__item'>
+                <a class='gallery__link'
+                    href=''>
+                    <img 
+                        class='gallery__img'
+                        src='${preview}'
+                        data-source='${original}'
+                        alt='${description}'
+                    />
+                </a>
+            </div>`;
+    })
+        .join('');
+}
+
+
+const picturePopper = (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery")) return;
+    const highResolutionPicLink = event.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+    <img src="${highResolutionPicLink}"width="800" height="600">`);
+
+  instance.show();
+};
+
+galleryContainer.addEventListener("click", picturePopper);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImgGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createImgGallery(item) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a 
+            class="gallery__item" 
+            href="${original}">
+        <img    
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"
+            width="800"
+            height="600" />
+    </a>`;
+    })
+    .join("");
+}
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition:"bottom",
+    captionDelay: 250,
+    captionType: "alt",
+    docClose: "true",
+});
